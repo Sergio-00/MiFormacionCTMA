e984401d3c932d19d87528ed7665d4e2fa703e0f
Andres Felipe Vargas Metrio <elgraffitero@gmail.com>
2026-08-19 10:24:19 -0500

    refactor: eliminar proyecto antiguo

--- app/src/main/java/com/joel/mi_formacion_ctma/model/EstadoActividad.kt
+++
@@ -1,8 +0,0 @@
-package com.joel.mi_formacion_ctma.model
-
-enum class EstadoActividad {
-    PENDIENTE,
-    EN_PROCESO,
-    COMPLETADA,
-    VENCIDA
-}
--- app/src/main/java/com/joel/mi_formacion_ctma/model/Prioridad.kt
+++
@@ -1,7 +0,0 @@
-package com.joel.mi_formacion_ctma.model
-
-enum class Prioridad {
-    BAJA,
-    MEDIA,
-    ALTA
-}
